7053aa7bd90424a858fe2a5e3922263ad9717416
Zejneb Semic <zejneb.semic@stu.ibu.edu.ba>
2025-12-28 00:45:47 +0100

    Fix CORS preflight handling via htaccess

--- backend/index.php
+++ backend/index.php
@@ -1,9 +1,5 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-
 require 'vendor/autoload.php';
 require 'rest/services/AuthService.php';
 require 'rest/services/AdminService.php';
