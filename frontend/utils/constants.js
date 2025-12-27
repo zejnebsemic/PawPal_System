df4956bcaa0a751e4caac9e52ba0e23949fd4938
Zejneb Semic <zejneb.semic@stu.ibu.edu.ba>
2025-12-28 00:27:07 +0100

    Added updates

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -1,6 +1,6 @@
 let Constants = {
-   PROJECT_BASE_URL: "https://plankton-app-yu9b6.ondigitalocean.app/",
-   IMAGES_BASE_PATH: "/PawPal_System/frontend/assets/images/",
+   PROJECT_BASE_URL: "https://plankton-app-yu9b6.ondigitalocean.app",
+   IMAGES_BASE_PATH: "/assets/images",
    USER_ROLE: "user",
    ADMIN_ROLE: "admin"
 }
