5d3cf95e3ef45a19f3943d32e0ef53fb84cd9453
Zejneb Semic <zejneb.semic@stu.ibu.edu.ba>
2025-12-28 00:41:17 +0100

    Add CORS headers for frontend-backend communication

--- backend/index.php
+++ backend/index.php
@@ -1,5 +1,9 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+
 require 'vendor/autoload.php';
 require 'rest/services/AuthService.php';
 require 'rest/services/AdminService.php';
