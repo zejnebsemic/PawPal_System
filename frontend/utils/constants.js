368b49bb13a813c80fba7759b04a9882db3eb58f
Zejneb Semic <zejneb.semic@stu.ibu.edu.ba>
2025-12-28 00:04:10 +0100

    Set API base URL for milestone-5 test deployment

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -1,5 +1,5 @@
 let Constants = {
-   PROJECT_BASE_URL: "http://localhost/PawPal_System/backend/",
+   PROJECT_BASE_URL: "https://plankton-app-yu9b6.ondigitalocean.app",
    IMAGES_BASE_PATH: "/PawPal_System/frontend/assets/images/",
    USER_ROLE: "user",
    ADMIN_ROLE: "admin"
