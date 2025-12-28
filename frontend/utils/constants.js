e6e2d183e3152c972c868e4575ae8c5f0aa312d9
Zejneb Semic <zejneb.semic@stu.ibu.edu.ba>
2025-12-28 03:17:42 +0100

    images path corrected

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -1,6 +1,6 @@
 let Constants = {
    PROJECT_BASE_URL: "https://octopus-app-nq68m.ondigitalocean.app",
-   IMAGES_BASE_PATH: "/assets/images",
+   IMAGES_BASE_PATH: "assets/images",
    USER_ROLE: "user",
    ADMIN_ROLE: "admin"
 }
