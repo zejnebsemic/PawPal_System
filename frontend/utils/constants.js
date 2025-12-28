335b4e7bc6a1819ab07cb6353fd2b7461653f827
Zejneb Semic <zejneb.semic@stu.ibu.edu.ba>
2025-12-28 03:27:04 +0100

    Fixed constants.js

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -1,6 +1,6 @@
 let Constants = {
-   PROJECT_BASE_URL: "https://octopus-app-nq68m.ondigitalocean.app",
-   IMAGES_BASE_PATH: "assets/images",
+   PROJECT_BASE_URL: "https://octopus-app-nq68m.ondigitalocean.app/",
+   IMAGES_BASE_PATH: "https://seashell-app-aq5un.ondigitalocean.app/assets/images/",
    USER_ROLE: "user",
    ADMIN_ROLE: "admin"
 }
